760f6e15bb68d8b6e2d07e1598f58d4cc1f3ae90
boskom <boskoma@gmail.com>
2020-11-09 00:44:32 +0100

    Update config-god-mode.el

--- personal/config-god-mode.el
+++ personal/config-god-mode.el
@@ -1,8 +1,9 @@
 (require 'god-mode)
 
+;; toggle god-mode
+;;(global-set-key (kbd "<f1>") #'god-local-mode)
+;;(global-set-key (kbd "C-<f1>") #'god-local-mode)
 
-(global-set-key (kbd "<f1>") #'god-local-mode)
-(global-set-key (kbd "C-<f1>") #'god-local-mode)
 (global-set-key (kbd "M-g") #'god-local-mode)
 
 
@@ -10,6 +11,7 @@
 (setq god-exempt-major-modes nil)
 (setq god-exempt-predicates nil)
 
+;; this does not work, Control is still prepended
 (setq god-mode-enable-function-key-translation nil)
 
 (god-mode)
